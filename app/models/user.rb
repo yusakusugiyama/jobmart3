2610dec7bcc861bdeae936a4b9ec73f172e7b535
Yusaku Sugiyama <yusakusugiyama@gmail.com>
2016-05-24 20:10:31 +0900

    model/user.rbにuserの姓名を表示するメソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def name_method
+    "#{last_name} #{first_name}"
+  end
+
+  def name_kana_method
+    "#{last_name_kana} #{first_name_kana}"
+  end
 end
