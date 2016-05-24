28918588b1d0f52f46967c6a22adea6291d38434
Yusaku Sugiyama <yusakusugiyama@gmail.com>
2016-05-24 19:15:52 +0900

    userのサインアップ時に名前を登録できるように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   #deviseのサインアップで許可するパラメーターを指定
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:name]
+    devise_parameter_sanitizer.for(:sign_up) << [:name,:last_name,:first_name,:last_name_kana,:first_name_kana]
 
   end
 end
